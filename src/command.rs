9a83fac59138ce02c18a57174550ed576b2d7fda
low13 <119625580+low13@users.noreply.github.com>
2023-07-14 20:42:07 -0300

    Early release version

--- src/command.rs
+++ src/command.rs
@@ -1,6 +1,32 @@
 use std::io::{self, Write};
 use std::{process, env, fs};
 use std::path::Path;
+use std::collections::HashMap;
+
+pub fn help() {
+    println!("\nCOMMANDS");
+    let mut commands_help: HashMap<&str, &str> = HashMap::new();
+
+    commands_help.insert("echo", "Prints the given text to the output");
+    commands_help.insert("cd", "Changes the current working directory to the specified path");
+    commands_help.insert("clear", "Clears all text in the window");
+    commands_help.insert("ls", "Lists all items in the current working directory");
+    commands_help.insert("mkfile", "Creates the given file if it doesn't exist yet");
+    commands_help.insert("rmfile", "Permanently removes the given file from the file system");
+    commands_help.insert("mkdir", "Creates the given directory if it doesn't exist yet");
+    commands_help.insert("rmdir", "Permanently removes the given directory if empty, using '--force' also removes non-empty directories");
+    commands_help.insert("read", "Reads the text contained in a file with UTF-8 encoding");
+    commands_help.insert("copy", "Copies content from path A to path B");
+    commands_help.insert("move", "Moves content from path A to path B, can be used to rename items");
+    commands_help.insert("find", "Prints the path for the given executable if found on the PATH variable");
+    commands_help.insert("run", "Attempts to run the given file with the given arguments, can be used to execute external commands");
+    commands_help.insert("help", "Lists all commands");
+    commands_help.insert("exit", "Exit the shell");
+
+    for (cmd, desc) in commands_help {
+        println!("{}: {}", cmd, desc);
+    }
+}
 
 pub fn exit() {
     process::exit(0);
@@ -159,4 +185,46 @@ pub fn mv(args: Vec<&str>) {
     }
 
     let _ = fs::rename(src_path, dest_path);
+}
+
+pub fn find(args: Vec<&str>) {
+    if args.len() == 0 {
+        println!("Expected path");
+        return;
+    }
+
+    if let Ok(path_var) = env::var("PATH") {
+        let mut file_name = args[0..].join(" ");
+        if !file_name.ends_with(".exe") {
+            file_name += ".exe";
+        }
+        let mut finished = false;
+
+        for path in env::split_paths(&path_var) {
+            let candidate = path.join(file_name.clone());
+            if candidate.is_file() {
+                finished = true;
+                println!("{}", candidate.display());
+                break;
+            }
+        }
+        if finished == false {
+            println!("Unable to find file {}", args[0..].join(" "));
+        }
+    }
+}
+
+pub fn run(args: Vec<&str>) {
+    if args.len() == 0 {
+        println!("Expected file");
+        return;
+    }
+
+    let result = process::Command::new(args[0]).args(&args[1..]).status();
+
+    if let Ok(status) = result {
+        if status.success() {
+            return;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,21 @@ use std::io::{self, Write};
 mod command;
 
 fn main() {
-    println!("SCS System Command Shell 0.2.1");
+    println!("SCS System Command Shell 1.0.0");
+    println!("Enter 'help' for a list of all commands");
+
     loop {
-        print!("{}> ", env::current_dir().unwrap().display());
+        print!("{} => ", env::current_dir().unwrap().display());
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
+        let mut invalid_input = false;
+        let _ = io::stdin().read_line(&mut input).map_err(|_| {
+            eprintln!("\nInvalid input found");
+            invalid_input = true;
+        });
+
+        if invalid_input { continue };
 
         let cmd = {
             let cmd_opt = input.split_whitespace().next();
@@ -23,6 +31,7 @@ fn main() {
         let args = input.split_whitespace().skip(1).collect::<Vec<&str>>();
 
         match cmd.as_str() {
+            "help" => command::help(),
             "exit" => command::exit(),
             "clear" => command::clear(),
             "echo" => command::echo(args),
@@ -35,6 +44,8 @@ fn main() {
             "read" => command::read(args),
             "copy" => command::copy(args),
             "move" => command::mv(args),
+            "find" => command::find(args),
+            "run" => command::run(args),
             _ => println!("Command not found: {}", cmd)
         }
     }
