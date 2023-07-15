68ef1528bbca11b159ec3179714b9ee6c4e4830e
low13 <119625580+low13@users.noreply.github.com>
2023-07-14 23:05:18 -0300

    Build script fix

--- build.rs
+++ build.rs
@@ -2,8 +2,10 @@ use std::fs;
 use std::process::Command;
 
 fn main() {
-    let _ = Command::new("cargo").args(["build", "--release"]).status();
-    let _ = fs::copy("target/release/scs.exe", "out/scs.exe");
+    let build_status = Command::new("cargo").args(["build", "--release", "--no-run"]).status();
+    if build_status.is_ok() {
+        let _ = fs::copy("target/release/scs.exe", "out/scs.exe");
+    }
 
     let _ = fs::create_dir("out");
     let _ = fs::create_dir("out/commands");
